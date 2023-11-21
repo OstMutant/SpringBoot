1a1f554e65441ab247c80310f547b3a120a1421b
Ostap Seniuk <ostap_seniuk@epam.com>
2023-11-21 21:14:10 +0200

    Timed annotation adding https://bootcamptoprod.com/measure-api-response-time-spring-boot/

--- src/main/java/org/ost/investigate/springboot/examples/rest/GreetingController.java
+++ src/main/java/org/ost/investigate/springboot/examples/rest/GreetingController.java
@@ -1,18 +1,19 @@
 package org.ost.investigate.springboot.examples.rest;
 
+import io.micrometer.core.annotation.Timed;
 import lombok.extern.slf4j.Slf4j;
 import org.ost.investigate.springboot.examples.aop.LogExecutionTime;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-@RestController
 @Slf4j
+@RestController
 public class GreetingController {
 
     @GetMapping("/greeting")
     @LogExecutionTime
-//    @Timed("my.metric.name")
+    @Timed(value = "api.greeting.timer", description = "Time taken to process my API endpoint")
     public Mono<String> greeting() {
         log.info("Greetings from Spring Boot!");
         return Mono.just("Greetings from Spring Boot!");
