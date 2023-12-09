429068021f596fd226a9b29d915118a7ee1a90bb
Ostap Seniuk <ostap_seniuk@epam.com>
2023-12-09 17:55:26 +0200

    Redis implementation updating

--- src/main/java/org/ost/investigate/springboot/examples/Application.java
+++ src/main/java/org/ost/investigate/springboot/examples/Application.java
@@ -1,6 +1,5 @@
 package org.ost.investigate.springboot.examples;
 
-import org.ost.investigate.springboot.examples.aop.LogExecutionTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
