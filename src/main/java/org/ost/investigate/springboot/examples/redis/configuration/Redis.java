b8581d514c024000f947c5e4392beb5f721a3d31
Ostap Seniuk <ostap_seniuk@epam.com>
2023-12-11 02:49:04 +0200

    refactoring

--- src/main/java/org/ost/investigate/springboot/examples/redis/configuration/Redis.java
+++ src/main/java/org/ost/investigate/springboot/examples/redis/configuration/Redis.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 // https://www.baeldung.com/spring-data-redis-tutorial
 @Configuration
+@EnableRedisRepositories(basePackages = "org.ost.investigate.springboot.examples.redis.repo")
 public class Redis {
 
     @Bean
